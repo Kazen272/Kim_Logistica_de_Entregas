b354d3dc716881bcbc540455e7975fa8b2aa6e5e
Kazen272 <cadudps@gmail.com>
2022-09-05 07:17:50 -0300

    Validação do login ok

--- sistema/login.php
+++ sistema/login.php
@@ -5,22 +5,27 @@ require ('../app/database/connect.php');
 
 //passo 1 seria receber esses dados via post e atribuir a uma variavel para conseguir manipular
 
-$email = $_POST['email'];
-$senha = MD5 ($_POST['senha']);
+//$email = $_POST['email'];
+//$senha = MD5 ($_POST['senha']);
 $active = 1; 
-
+$email = "admin@admin.com.br";
+$senha = "1234";
 
 $validacao_login = "SELECT `user_email`,`user_password`,`user_active` 
 FROM user 
-WHERE `user_email`='$email' AND `user_password`='$senha' AND `user_active`='$active';";
+WHERE `user_email`='$email' AND `user_password`=$senha AND `user_active`=$active;";
 
 $result_login = $pdo->prepare($validacao_login);
 $result_login -> execute();
 
 ($row_login = $result_login->fetch(PDO::FETCH_ASSOC));
+$num_linhas = $result_login->rowCount();
 
-if  ($row_login == 1) {
-    echo "Você Entrou";
+//var_dump($num_linhas);
+
+
+if  ($num_linhas == 1) {
+   
 } 
 
 
