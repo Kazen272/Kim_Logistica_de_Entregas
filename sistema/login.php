35eb907acee858e77bbe8852e2678341047d2cbf
Kazen272 <cadudps@gmail.com>
2022-09-05 03:03:55 -0300

    atualizações

--- sistema/login.php
+++ sistema/login.php
@@ -1,2 +1,13 @@
+<?php
+
+require ('../app/database/connect.php');
+
+
+//passo 1 seria receber esses dados via post e atribuir a uma variavel para conseguir manipular
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+$active = 1; 
+
 
 
