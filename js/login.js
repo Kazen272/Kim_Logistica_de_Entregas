60802763d5f1f2fb8cefaa820096f58dbd0bb525
Kazen272 <cadudps@gmail.com>
2022-09-05 17:42:27 -0300

    Implementação de loading, avanço no código do backend, estruturação correta do ajax

--- js/login.js
+++ js/login.js
@@ -1,17 +1,58 @@
 $('document').ready(function(){
  
-	$("#btn-entrar").click(function(){
-		var email = $("#email").val();
-		var senha = $("#senha").val();
-			
+	$("#btn-login").click(function(){
+		var email = $("#login-email").val();
+		var senha =$("#login-senha").val();
 		$.ajax({
 			type : 'POST',
 			url  : 'login.php',
-			data : {
+			data :{
 				email:email,
 				senha:senha
+			}, 
+			dataType: 'json',
+			beforeSend: function()
+			{	
+				$("#loading").html("<img id='loading-png' src='../img/loading.png' width='100' height='50'>");
+			},
+			success :  function(response){						
+				if(response.codigo == "1"){	
+					$("#btn-login").html('Entrar');
+					$("#login-alert").css('display', 'none')
+					window.location.href = "../sistema/dashboard.html";
+				} else
+					{			
+						$("#btn-login").html('Entrar');
+						$("#login-alert").css('display', 'block')
+						$("#mensagem").html('<strong>Erro! </strong>' + response.mensagem);
+					}
 			}
 		});	
 	});
  
 });
+
+
+
+
+
+
+
+/*
+
+dataType: 'json',
+			beforeSend: function()
+			{	
+				$("#loading").html("<img src='../img/loading.png'>");
+			},
+			success :  function(response){						
+				if(response.codigo == "1"){	
+					$("#btn-login").html('Entrar');
+					window.location.href = "home.php";
+				}
+				else{			
+					$("#btn-login").html('Entrar');
+					$("#mensagem").html('<strong>Erro! </strong>' + response.mensagem);
+				}
+		    }
+*/
--- sistema/login.php
+++ sistema/login.php
@@ -1,24 +1,34 @@
 <?php
 
+//conexão com o banco
 require ('../app/database/connect.php');
+
+//
 
 
 //passo 1 seria receber esses dados via post e atribuir a uma variavel para conseguir manipular
 
-//$email = $_POST['email'];
-//$senha = MD5 ($_POST['senha']);
-$active = 1; 
-$email = "admin@admin.com.br";
-$senha = "1234";
+//
+$email = $_POST['email']; 
+$senha = MD5 ($_POST['senha']);
+$active =1;
+//valida se email e senha não vieram vazios
+if (empty($email) || empty($senha)):
+    $resposta[] = ['mensagem' => "Email e Senha precisam ser preenchidos"];
+    echo json_encode($resposta);
+    exit();
+endif;
 
+
+//Validação do login no bd
 $validacao_login = "SELECT `user_email`,`user_password`,`user_active` 
 FROM user 
-WHERE `user_email`='$email' AND `user_password`=$senha AND `user_active`=$active;";
+WHERE `user_email`='$email' AND `user_password`='$senha' AND `user_active`='$active';";
 
 $result_login = $pdo->prepare($validacao_login);
 $result_login -> execute();
 
-($row_login = $result_login->fetch(PDO::FETCH_ASSOC));
+
 $num_linhas = $result_login->rowCount();
 
 //var_dump($num_linhas);
